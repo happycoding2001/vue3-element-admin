727b8773fdc200bd18bed34569b6121a6ed739d3
dingming <ddd>
2024-01-03 16:14:53 +0800

    save

---
+++ src/utils/auth.ts
@@ -0,0 +1,16 @@
+// src/utils/auth.ts
+import Cookies from 'js-cookie';
+ 
+const TokenKey = 'vue3-element-admin-token';
+ 
+export function getToken() {
+  return Cookies.get(TokenKey);
+}
+ 
+export function setToken(token: string) {
+  Cookies.set(TokenKey, token);
+}
+ 
+export function removeToken() {
+  return Cookies.remove(TokenKey);
+}
