a220592e7cf2d107e2d56a12ccb421741943025d
dingming <ddd>
2024-01-03 17:28:37 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div>home</div>
+  <div>
+    <svg-icon icon-class="jd" />
+  </div>
   <el-button @click="handleLogin" type="primary">确定</el-button>
 </template>
 
 <script setup lang="ts">
+import SvgIcon from "@/components/SvgIcon/index.vue";
 import { loginApi } from "@/api/auth";
 // import.meta.env. 智能提示
 const handleLogin = () => {
