b071381f032a3981811a3a81e66c62386b8910e9
dingming <ddd>
2024-01-03 11:18:56 +0800

    三、路径别名

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
 
 <template>
   <div>home</div>
   <el-button type="primary">确定</el-button>
+  <HelloWorld msg="Vite + Vue" />
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
