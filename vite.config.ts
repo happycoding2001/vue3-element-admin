149a1c5421501df0f0461a7cfef75968a44909c8
dingming <ddd>
2024-01-03 12:06:44 +0800

    五、反向代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,35 @@
-import { defineConfig } from 'vite'
+// vite.config.ts
+import {UserConfig, ConfigEnv, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 import path from 'path'
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-        "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
-    }
-  }
-})
+ 
+export default ({command, mode}: ConfigEnv): UserConfig => {
+  // 获取 .env 环境配置文件
+  const env = loadEnv(mode, process.cwd())
+ 
+  return (
+      {
+        plugins: [
+          vue()
+        ],
+        // 本地反向代理解决浏览器跨域限制
+        server: {
+          host: 'localhost',
+          port: Number(env.VITE_APP_PORT),
+          open: true, // 启动是否自动打开浏览器
+          proxy: {
+            [env.VITE_APP_BASE_API]: {
+              target: 'http://vapi.youlai.tech', // 有来商城线上接口地址
+              changeOrigin: true,
+              rewrite: path => path.replace(new RegExp('^' + env.VITE_APP_BASE_API), '')
+            }
+          }
+        },
+        resolve: {
+          alias: {
+            "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
+          }
+        }
+      }
+  )
+}
